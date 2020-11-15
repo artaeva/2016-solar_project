5c2aa99b9e934d952e1780bfe7c39d7878393e32
Artaeva Alvina <artaeva.aa@phystech.edu>
2020-11-15 19:47:16 +0300

    что-то не так

--- solar_main.py
+++ solar_main.py
@@ -34,9 +34,10 @@ def execution():
     """
     global physical_time
     global displayed_time
+    global space
     recalculate_space_objects_positions(space_objects, time_step.get())
     for body in space_objects:
-        update_object_position(space, body)
+        space = update_object_position(space, body)
     physical_time += time_step.get()
     displayed_time.set("%.1f" % physical_time + " seconds gone")
     if perform_execution:
